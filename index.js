162626026e1c038f18563cffd91e340c9f877c61
Glen Keane <glenkeane.94@gmail.com>
2018-02-25 20:00:58 +0000

    use new tools written to simplify process

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const loader = require('initialise-wasm')
 const buf = require('./build/wasm.js')
 
-module.exports = new WebAssembly.Instance(WebAssembly.Module(buf)).exports
+module.exports = loader(buf, { promise: false }).exports
